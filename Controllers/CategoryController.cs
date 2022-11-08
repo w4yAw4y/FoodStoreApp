32108b06a0bedde46de79d52c69c3f3608dfed07
308 <308@308-4>
2022-11-08 12:14:49 +0300

    1 Практика +

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using FoodStoreApp.Data;
+using FoodStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodStoreApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // Переменная контекста для доступа к базе данных
+        private readonly ApplicationDbContext _db;
+
+        // Внедряем зависимость ApplicationDbContext через конструктор
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // Создаем список записей категорий из базы данных
+            IEnumerable<Category> categoryList = _db.Category;
+            // Возвращаем загруженный список в представление
+            return View(categoryList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+
+            _db.Category.Add(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var cat = _db.Category.Find(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
